5a2d5b35795373dc2665cf2e6e53aca6455f85f4
Marie Lamoureux <69575238+lamoureuxmarie@users.noreply.github.com>
2021-06-16 21:14:33 +0200

    added Synonym element

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import Synonyms from './Synonyms';
 
 export default function Meaning(props) {
     return (
         <div className="Meaning">
         <h3>{props.meaning.partOfSpeech}</h3>
-        <p>{props.meaning.definitions[0].definition}</p>
-        <p>{props.meaning.definitions[0].example}</p>
+        {props.meaning.definitions.map(function(definition, index) {
+                  return (
+                    <div key={index}>
+                     <p>
+                     {definition.definition}
+                     <br />
+                     <em>{definition.example}</em>
+                     <br />
+                     <Synonyms synonyms={definition.synonyms} />
+                     </p>
+                     </div>
+                    );
+              })}
+   
+    
         </div>
-    )
-       
+    );       
 }
