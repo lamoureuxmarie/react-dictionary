231455f07d81920a973080bd700206b6cd7cbec5
Marie Lamoureux <69575238+lamoureuxmarie@users.noreply.github.com>
2021-06-20 18:02:31 +0200

    added FontAwesome icons

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./Phonetics.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faVolumeUp } from '@fortawesome/free-solid-svg-icons'
 
 export default function Phonetics (props) {
     return (
         <div className="Phonetics">
-            <a href={props.phonetic.audio} target="_blank">Listen</a>
+            <a href={props.phonetic.audio} target="_blank"><FontAwesomeIcon icon={faVolumeUp} /></a>
         
             <span className="text">{props.phonetic.text}</span>
         </div>
